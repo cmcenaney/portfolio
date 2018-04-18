d85b5f956c6bf31e3ee6e03e53fefceea23647a2
McenaneyC <colleen.mcenaney@dowjones.com>
2018-04-18 16:48:52 +0100

    updates

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
 $(document).ready(function(){
 	$.getJSON('projects.json', function(data){
 		
-
+		var d4c = _.filter(data, function(d){ return d.org == 'D4C' });
 		var wsj = _.filter(data, function(d) { return d.org == 'WSJ' });
 		var nyt = _.filter(data, function(d) { return d.org == 'NYT' });
 		var cc = _.filter(data, function(d) { return d.org == 'CC'});
 
-		var projects = {"wsj": wsj, "nyt": nyt, "cc": cc};
+		var projects = {"d4c": d4c, "wsj": wsj, "nyt": nyt, "cc": cc};
+
+		console.log(projects)
 
 		var template = $("#template").html();
 		var html = Mustache.to_html(template, projects);
